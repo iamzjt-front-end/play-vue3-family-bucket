88480f249e1dad988006ee639a07a132be05446a
iamzjt-front-end <iamzjt@foxmail.com>
2023-04-10 14:26:27 +0800

    🔥 feat: 引入vuex及基础使用

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import store from './store/index';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(store).mount('#app');
