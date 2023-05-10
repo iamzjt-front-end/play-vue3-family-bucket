adc05a991c82faa4b600bd67d0593ece06d74aee
iamzjt-front-end <iamzjt@foxmail.com>
2023-05-10 13:05:56 +0800

    🔥 feat: mini-router init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css';
 import App from './App.vue';
 import store from './store/index';
 
+
 const app = createApp(App);
 
 app.use(store).mount('#app');
---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import { createRouter, createWebHashHistory } from "../mini/mini-router.js";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
